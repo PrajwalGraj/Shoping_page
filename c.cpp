db586a1051fafafcedfc50925dcb93b7c50cc9ca
Prajwal G <prajjuprajwal09@gmail.com>
2023-05-10 13:27:22 +0530

    Shoping page

---
+++ c.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fo(n) for (int i = 0; i < n; i++)
+#define ll long long int
+// solution
+// prajwal
+// solution for prob
+int main(){
+    int t;
+    cin>>t;
+    if(t%2==0)cout<<"yes"<<endl;
+    else cout<<"NO"<<endl;
+
+    
+    
+
+    
+}
---
+++ text.js
@@ -0,0 +1,37 @@
+// console.log("Hello World")
+
+// var b= "World"
+// var c=b.replace('world','hello')
+// if(a>5){
+//     console.log("greaater than 10");
+// }else if(a==10){
+//     console.log('the number is 10');
+// }else{
+//     console.log('the number is less than 5 and not equial to 10');
+// }
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+// while (condition) {
+// var a=11;
+// var a=13;
+// let a=12;
+// let a=[5,3,7,2,1,"hello"];
+// console.log(a);
+// a.push("world")
+// // a.sort()
+// console.log(a[2]);
+
+// let head=document.getElementsByClassName('head');
+// console.log(head);
+
